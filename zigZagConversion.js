3c6dd45d904e62d526a7fc37a51573200f45f918
Jeremy Wood <js.wood206@gmail.com>
2021-08-02 21:05:54 -0700

    In progress

---
+++ zigZagConversion.js
@@ -0,0 +1,15 @@
+var convert = function(s, numRows) {
+    
+    
+    // takes in string, return string read diagonally as visualized below. 
+    // string actually reads "pahnaplsiigyir" coming in
+
+    // create function that un jumbles string, based on number of rows
+
+
+    
+};
+
+P   A   H   N
+A P L S I I G
+Y   I   R
