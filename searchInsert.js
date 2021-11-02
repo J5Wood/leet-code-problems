af5969174c4332a10ef0abf8fdfe9cc6b148c9b1
Jeremy Wood <68167282+J5Wood@users.noreply.github.com>
2021-11-01 18:08:44 -0700

    Completed

---
+++ searchInsert.js
@@ -0,0 +1,11 @@
+var searchInsert = function(nums, target) {
+    let i
+    for(i = 0; i < nums.length && nums[i] <= target; i++){
+        if(nums[i] === target){
+            return i
+        }
+    }
+    return i
+};
+
+console.log(searchInsert([1,3,5,6], 5))
