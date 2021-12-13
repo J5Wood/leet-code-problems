55c37817557de272c51f88b625bda34d32ef8e16
Jeremy Wood <68167282+J5Wood@users.noreply.github.com>
2021-12-12 23:39:13 -0800

    Completed

---
+++ validParentheses.js
@@ -0,0 +1,81 @@
+// ***** my attempt
+
+let isValid = function(s) {
+    let checkStack = []
+    let counter = -1
+    for(let i = 0; i < s.length; i++){
+        if(counter === -1){
+            checkStack.push(s[i])
+            counter++
+        } else if(s[i] === "}") {
+            if(checkStack[counter] !== "{") return false
+            checkStack.pop()
+            counter--
+        } else if (s[i] === "]") {
+            if(checkStack[counter] !== "[") return false
+            checkStack.pop()
+            counter--
+        } else if (s[i] === ")") {
+            if(checkStack[counter] !== "(") return false
+            checkStack.pop()
+            counter--
+        } else {
+            checkStack.push(s[i])
+            counter++
+        }
+    }
+    return counter === -1 ? true : false
+};
+
+
+//  ***** examples
+
+// let isValid = function(s) {
+//     while (
+//       s.indexOf("{}") !== -1 ||
+//       s.indexOf("[]") !== -1 ||
+//       s.indexOf("()") !== -1
+//     ) {
+//       s = s.replace("()", "");
+//       s = s.replace("{}", "");
+//       s = s.replace("[]", "");
+//     }
+//     return s === "";
+//   };
+
+  // * --------------------------
+
+//   var isValid = function(s) {
+        
+//     if (s.length ==0 || s.length%2 != 0){
+//         return false;
+//     }
+    
+//     const closingParentheses = Object.freeze({
+//         "(": ")",
+//         "{": "}",
+//         "[": "]"
+//     });
+    
+//     const stack = [];
+    
+//     for (let i=0; i<s.length; i++) {
+//         if (s[i] in closingParentheses) {
+//             stack.push(s[i]);
+//         }else if( s[i] != closingParentheses[stack.pop()]) {
+//             return false;
+//         }
+//     }
+    
+//     return stack.length == 0;
+// };
+
+// * ----------------------
+
+
+
+console.log(isValid("()"))
+console.log(isValid("()[]{}"))
+console.log(isValid("(]"))
+console.log(isValid("([)]"))
+console.log(isValid("{[]}"))
