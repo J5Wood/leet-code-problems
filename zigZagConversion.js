61afd39aa451533fee23efcd8e00395e9f4d7958
Jeremy Wood <68167282+J5Wood@users.noreply.github.com>
2022-01-03 15:48:21 -0800

    Completed

--- zigZagConversion.js
+++ zigZagConversion.js
@@ -1,15 +1,22 @@
+// Just keeps track of individual rows, tracks each row as index, loops through string, index just moves up then down from 0 to height -1 to put current letter in correct row.
+
 var convert = function(s, numRows) {
     
-    
-    // takes in string, return string read diagonally as visualized below. 
-    // string actually reads "pahnaplsiigyir" coming in
+    let returnArr = []
+    let index = 0
+    let step = 1
+    for(let i = 0; i < s.length; i++){
+        if(returnArr[index] === undefined) returnArr[index] = ""
+        returnArr[index]+= s[i]
+        if(index === 0){
+            step = 1
+        } else if(index === numRows - 1){
+            step = -1
+        }
+        index+= step
+    }
+    return returnArr.join("")
+};  
 
-    // create function that un jumbles string, based on number of rows
-
-
-    
-};
-
-// P   A   H   N
-// A P L S I I G
-// Y   I   R
+console.log(convert("PAYPALISHIRING", 3))
+console.log(convert("PAYPALISHIRING", 4))
